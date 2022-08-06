f4e2ed0e358afaf1bd51a4ec834cbb8f90b6c0ba
MahdMahd <smmehdikhadem@gmial.com>
2022-08-06 18:02:33 +0430

    first commit

---
+++ sphinx/conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'antibioticmaster'
+copyright = '2022, MehdiKhadem'
+author = 'MehdiKhadem'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+import sys
+import os
+import shlex
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'fa'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_minoo_theme"
+
+html_theme_path = ["_templates"]
---
+++ sphinx/index.rst
@@ -0,0 +1,20 @@
+.. antibioticmaster documentation master file, created by
+   sphinx-quickstart on Sat Aug  6 17:15:19 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to antibioticmaster's documentation!
+============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
